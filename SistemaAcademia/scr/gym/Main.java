4ef817ad83612734db301c8f8c9990bf9daa9ed1
Valter Junior <14valterjunior@gmail.com>
2025-08-20 12:28:01 -0300

    testes da classe Treino.

--- SistemaAcademia/scr/gym/Main.java
+++ SistemaAcademia/scr/gym/Main.java
@@ -3,11 +3,13 @@ package scr.gym;
 import scr.gym.model.usuario.aluno.Aluno;
 import scr.gym.model.usuario.instrutor.Instrutor;
 import scr.gym.model.usuario.plano.Plano;
+import scr.gym.model.usuario.treino.Treino;
 
 public class Main {
     public static void main(String[] args) {
         Aluno usuario = new Aluno("Pedro", "pedro@example.com", "senha123", "12345");
         Instrutor instrutor = new Instrutor("Maria", "maria@example.com", "senha456", "Musculação");
+        Treino treino = new Treino("Treino para hipertrofia", "Iniciante", 60);
         Plano plano = new Plano("Premium", 500.0, 12);
         System.out.println("Nome: " + usuario.getNome());
         System.out.println("Email: " + usuario.getEmail());
@@ -20,5 +22,8 @@ public class Main {
         System.out.println("Plano de Treino: " + plano.getNomePlano());
         System.out.println("Valor do Plano: R$" + plano.getValor());
         System.out.println("Duração em meses: " + plano.getDuracaoMeses());
+        System.out.println("Descrição do treino: " + treino.getDescricaoTreino());
+        System.out.println("Nivel de dificuldade do treino: " + treino.getNivelDificuldade());
+        System.out.println("Deuração em minutos do treino: " + treino.getDuracaoMinutos());
     }
 }
