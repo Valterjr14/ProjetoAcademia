e73031d928015c3a03d0ebf0e5584bf5cbc5c39a
Valter Junior <14valterjunior@gmail.com>
2025-08-20 11:50:17 -0300

    adicionado classe Plano.

---
+++ SistemaAcademia/scr/gym/model/usuario/plano/Plano.java
@@ -0,0 +1,37 @@
+package scr.gym.model.usuario.plano;
+
+public class Plano {
+    private String nomePlano;
+    private double valor;
+    private int duracaoMeses;
+
+    public Plano(String nomePlano, double valor, int duracaoMeses){
+        this.nomePlano = nomePlano;
+        this.valor = valor;
+        this.duracaoMeses = duracaoMeses;
+    }
+
+    public String getNomePlano(){
+        return nomePlano;
+    }
+
+    public void setNomePlano(String nomePlano){
+        this.nomePlano = nomePlano;
+    }
+
+    public double getValor(){
+        return valor;
+    }
+
+    public void setValor(double valor){
+        this.valor = valor;
+    }
+
+    public int getDuracaoMeses(){
+        return duracaoMeses;
+    }
+
+    public void setDuracaoMeses(int duracaoMeses){
+        this.duracaoMeses = duracaoMeses;
+    }
+}
