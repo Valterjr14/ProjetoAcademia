deda7dd3c46d927360004a9805d8f11a0cf97bc3
Valter Junior <14valterjunior@gmail.com>
2025-08-20 11:56:20 -0300

    testes da classe plano.

--- SistemaAcademia/scr/gym/Main.java
+++ SistemaAcademia/scr/gym/Main.java
@@ -2,11 +2,13 @@ package scr.gym;
 
 import scr.gym.model.usuario.aluno.Aluno;
 import scr.gym.model.usuario.instrutor.Instrutor;
+import scr.gym.model.usuario.plano.Plano;
 
 public class Main {
     public static void main(String[] args) {
         Aluno usuario = new Aluno("Pedro", "pedro@example.com", "senha123", "12345");
         Instrutor instrutor = new Instrutor("Maria", "maria@example.com", "senha456", "Musculação");
+        Plano plano = new Plano("Premium", 500.0, 12);
         System.out.println("Nome: " + usuario.getNome());
         System.out.println("Email: " + usuario.getEmail());
         System.out.println("Senha: " + usuario.getSenha());
@@ -15,5 +17,8 @@ public class Main {
         System.out.println("Email: " + instrutor.getEmail());
         System.out.println("Senha: " + instrutor.getSenha());
         System.out.println("Especialidade: " + instrutor.getEspecialidade());
+        System.out.println("Plano de Treino: " + plano.getNomePlano());
+        System.out.println("Valor do Plano: R$" + plano.getValor());
+        System.out.println("Duração em meses: " + plano.getDuracaoMeses());
     }
 }
